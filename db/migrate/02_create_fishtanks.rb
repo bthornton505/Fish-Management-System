ac01f425d7f91e73e850afc57f3f0b95c8c1fbc0
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-31 17:45:47 +0000

    Created migration for fishtank table

---
+++ db/migrate/02_create_fishtanks.rb
@@ -0,0 +1,11 @@
+class CreateFishtanks < ActiveRecord::Migration
+  
+  def change 
+    create_table :fishtanks do |t|
+      t.integer :gallons
+      t.integer :fish_capacity
+      t.integer :user_id 
+      # t.integer :fish_id
+    end 
+  end 
+end 
