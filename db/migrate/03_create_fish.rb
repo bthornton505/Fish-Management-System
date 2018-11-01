d04897bbe095cb70b44039f0d97cc60593d98084
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-01 15:50:53 +0000

    removed the quantity column

--- db/migrate/03_create_fish.rb
+++ db/migrate/03_create_fish.rb
@@ -1,9 +1,8 @@
-class CreateFish < ActiveRecord::Migration
+class CreateFish < ActiveRecord::Migration[4.2]
   
   def change 
     create_table :fish do |t|
       t.string :species
-      t.integer :quantity
       t.integer :fishtank_id
     end 
   end 
