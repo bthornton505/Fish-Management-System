6b259663ab73dc874e8151719f8b393b59f9c7ad
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-01 19:02:30 +0000

    added routes for signup, profile, and logout... they don't all work though. ugh.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController 
   
+  get '/profile' do 
+    @user = User.find_by(params[:username])
+    erb :'users/profile'
+  end 
+  
   get '/signup' do 
     erb :'users/signup'
+  end 
+  
+  post '/signup' do 
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else 
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save 
+      session[:user_id] = @user.id 
+      redirect to '/profile'
+    end 
+  end 
+  
+  get '/logout' do 
+    logout!
+    redirect to '/login'
   end 
   
 end 
