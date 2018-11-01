39d20f5005956d33201ac487a7d45bb713b5b6e4
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-01 15:48:45 +0000

    added an instance method count_by_species

--- app/models/fishtank.rb
+++ app/models/fishtank.rb
@@ -2,4 +2,14 @@ class Fishtank < ActiveRecord::Base
   belongs_to :user 
   has_many :fish 
   
+  def count_by_species(species)
+    count = 0 
+    fish.each do |f|
+      if species == f.species 
+        count+= 1 
+      end 
+    end
+    count
+  end 
+  
 end 
