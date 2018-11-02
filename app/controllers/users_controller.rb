d8a977b1fb5092bee1b7af0ac9a2b1eab312bbf2
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-02 20:27:06 +0000

    signup still doesn't redirect to profile page but home page can go to profile and back to home page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController 
   
-  get '/profile/:slug' do 
+  get '/users/:slug' do 
     @user = User.find_by_slug(params[:slug])
     erb :'/users/profile'
   end 
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save 
       session[:user_id] = @user.id 
-      redirect to '/profile/:slug'
+      redirect to 'users/profile'
     end 
   end 
   
