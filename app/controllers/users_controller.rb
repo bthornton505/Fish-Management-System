d44763161db4e4683656f91e41f448b84c1a494d
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-03 22:40:59 +0000

    finally got my signup feature to work and direct users to their new profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save 
       session[:user_id] = @user.id 
-      redirect to 'users/profile'
+      redirect to "/users/#{@user.slug}"
     end 
   end 
   
