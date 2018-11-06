811387604da82889e8cdd66b91b4b8f7187cb881
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-06 18:32:49 +0000

    added parameters to when creating a new fish... doesn't seem to help though.

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -7,10 +7,10 @@ class FishController < ApplicationController
   
   post '/new_fish' do 
     redirect_if_not_logged_in
-    if params[:species] == ""
+    if params[:species] == "" || params[:fishtank_id] == ""
       redirect to '/fish/new_fish'
     else 
-      @fish = Fish.new(:species => params[:species])
+      @fish = Fish.new(:species => params[:species], :fishtank_id => params[:fishtank_id])
       @fish.save 
       redirect to "/fish/#{@fish.id}"
     end 
