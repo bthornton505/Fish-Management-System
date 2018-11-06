ba1e12131026b44937695ef49cfc6fe6b9d9fba0
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-06 22:10:32 +0000

    created routes to edit a fish and a route to delete a fish

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -22,4 +22,39 @@ class FishController < ApplicationController
     erb :'fish/show_fish'
   end 
   
+  get '/fish/:id/edit' do 
+    redirect_if_not_logged_in
+    @fish = Fish.find_by_id(params[:id])
+    if @fish && @fish.fishtank.user == @current_user
+      erb :'/fish/edit_fish'
+    else 
+      redirect to '/'
+    end 
+  end 
+  
+  patch '/fish/:id' do 
+    redirect_if_not_logged_in
+    if params[:species] == "" || params[:fishtank_id] == ""
+      redirect to "/fish/#{params[:id]}/edit"
+    else 
+      @fish = Fish.find_by_id(params[:id])
+      if @fish && @fish.fishtank.user == @current_user
+        @fish.update(:species => params[:species], :fishtank_id => params[:fishtank_id])
+        redirect to "/fish/#{@fish.id}"
+      else 
+        redirect to "/fish/#{@fish.id}/edit"
+      end 
+    end 
+  end 
+  
+  post '/fish/:id/delete' do 
+    redirect_if_not_logged_in
+    @fish = Fish.find_by_id(params[:id])
+    if @fish && @fish.fishtank.user == @current_user
+      @fish.delete 
+      redirect to "/users/#{@current_user.slug}"
+    end 
+  end 
+  
+  
 end 
