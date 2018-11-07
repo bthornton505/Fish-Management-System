4be86173a1e48bb3527d1451af6962dfabe41f49
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-07 21:05:26 +0000

    added some potential validations to my user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_many :fishtanks
   has_secure_password 
   
+  validates :username, uniqueness: {case_sensitive: false, message: "That username is taken."}, presence: {message: "Username can't be blank."}
+  validates :email, uniqueness: {case_sensitive: false, message: "There is already an account with that email. Try logging in."}, presence: {message: "Email can't be blank."}
+
   def slug
     username.downcase.gsub(" ","-")
   end 
