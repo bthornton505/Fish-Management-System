25dce353463ebe318ef7c7cec68f016d4c5ec92f
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-08 19:55:38 +0000

    refactored the post route to create fishtanks

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -7,13 +7,14 @@ class FishController < ApplicationController
   
   post '/new_fish' do 
     redirect_if_not_logged_in
-    if params[:species] == "" || params[:fishtank_id] == ""
+    @fish = Fish.create(:species => params[:species], :fishtank_id => params[:fishtank_id])
+    
+    if !@fish.valid?
+      flash_error(@user)
       redirect to '/fish/new_fish'
-    else 
-      @fish = Fish.new(:species => params[:species], :fishtank_id => params[:fishtank_id])
-      @fish.save 
-      redirect to "/fish/#{@fish.id}"
-    end 
+    end  
+      
+    redirect to "/fish/#{@fish.id}"
   end 
   
   get '/fish/:id' do 
