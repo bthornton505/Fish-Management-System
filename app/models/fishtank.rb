188bcdf4d7e9d9cdda064905db74df99cc62be3c
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-08 19:43:04 +0000

    added validations to fishtank model

--- app/models/fishtank.rb
+++ app/models/fishtank.rb
@@ -2,14 +2,19 @@ class Fishtank < ActiveRecord::Base
   belongs_to :user 
   has_many :fishes
   
-  def count_by_species(species) # this method would allow you to add multiples of the same fish to a fishtank 
-    count = 0 
-    fish.each do |f|
-      if species == f.species 
-        count+= 1 
-      end 
-    end
-    count
-  end 
+  validates :name, presence: {message: "Name can't be blank."}
+  validates :gallons, presence: {message: "Name can't be blank."}
+  
+  
+  
+  # def count_by_species(species) # this method would allow you to add multiples of the same fish to a fishtank 
+  #   count = 0 
+  #   fish.each do |f|
+  #     if species == f.species 
+  #       count+= 1 
+  #     end 
+  #   end
+  #   count
+  # end 
   
 end 
